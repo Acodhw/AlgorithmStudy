eb7ef9b0015be340e87f4caee5669c9f4e5e6435
Chihwan Ahn <66577471+ChihwanAhn@users.noreply.github.com>
2023-06-21 13:27:34 +0900

    Set comment in Problem_1004_Solved.cpp

--- Problem 1004/Problem_1004_Solved.cpp
+++ Problem 1004/Problem_1004_Solved.cpp
@@ -13,16 +13,16 @@ int main()
         cin >> n;
         while(n > 0){
             cin >> cx >> cy >> r;
-            int d1 = (cx - x1) * (cx - x1) + (cy - y1) * (cy - y1);
-            int d2 = (cx - x2) * (cx - x2) + (cy - y2) * (cy - y2);
+            int d1 = (cx - x1) * (cx - x1) + (cy - y1) * (cy - y1); // get square of distance from circle centor to start point
+            int d2 = (cx - x2) * (cx - x2) + (cy - y2) * (cy - y2); // get square of distance from circle centor to arrival point
            
-            if(!(d1 <= r*r && d2 <= r*r)) {
-                if(d1 <= r*r) count++;
-                if(d2 <= r*r) count++;
+            if(!(d1 <= r*r && d2 <= r*r)) { // if both start and arrival point in circle => not go to out.
+                if(d1 <= r*r) count++; // if d1 < square of radious => start point is in circle
+                if(d2 <= r*r) count++; // if d1 < square of radious => start arrival is in circle
             }
             n--;
         }
-        cout << count << endl;
+        cout << count << endl; // print count
         t--;
     }
 
